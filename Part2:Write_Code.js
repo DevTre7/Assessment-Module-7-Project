d8b8642b1d5bf2cdf70edb06462d911befe41a28
Emmanuel Dada <Emmanuel.dada71@gmail.com>
2023-01-03 02:54:44 -0600

    inital upload

--- Part2:Write_Code.js
+++ Part2:Write_Code.js
@@ -1,4 +1,5 @@
-//1) Sum Zero
+//1) Sum Zero:
+
   //Nested For Loop:
 function addToZero(array) {
 for (let i = 0; i < array.length; i++){
@@ -17,6 +18,7 @@ return false;
 
 const arrayOne = [1,7,3];
 
+console.log(arrayOne);
 console.log(addToZero(arrayOne));
 
 // The Runtime for this function: O(n^2) - Quadratic Time
@@ -51,5 +53,52 @@ console.log(hasUniqueCharacters(word1));
 console.log(hasUniqueCharacters(word2));
 
 console.log("------------------------------------");
-
+// The Runtime for this function is: O(n^2) - Quadratic Time
 // "charAt() function": https://www.w3schools.com/java/ref_string_charat.asp
+
+//3)Pangram Sentence:
+
+function isPangram(sentence){
+  let sentenceArray = sentence.toLowerCase();
+  let englishAlphabetArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  
+  for (let i = 0; i < englishAlphabetArray.length; i++) {
+    if(sentenceArray.indexOf(englishAlphabetArray[i]) < 0) //
+    {
+      return false;
+    }
+  }
+  return true;
+};
+
+let sentence1 = "The quick brown fox jumps over the lazy dog!"
+let sentence2 = "I like cats, but not mice."
+let sentence3 = "Today is going to be great!"
+
+console.log(isPangram(sentence1));
+console.log(isPangram(sentence2));
+console.log(isPangram(sentence3));
+// console.log( sentence3)
+console.log("------------------------------------");
+// The Runtime for this function is: O(n)- Linear Time
+
+
+//4) Longest Word:
+
+function findTheLongestWord(array){
+  let longestWord = 0;
+  for(let i = 0; i < array.length; i++){
+    if(array[i].length > longestWord.length)
+    {
+      longestWord = wordList[i];
+    }
+  }
+  return longestWord.length;
+
+}
+
+let wordList = ["Hi", "Hello", "Morning", "Sunny", "Joy"]
+console.log(findTheLongestWord(wordList));
+console.log("------------------------------------");
+
+// The Runtime for this function is: O(1) - Constant Time
